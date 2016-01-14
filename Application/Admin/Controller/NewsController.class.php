b675aa66bb3d9798df9b03a48bee76d6de7eddf1
nanze <811046@qq.com>
2016-01-14 15:57:29 +0800

    ....

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -8,14 +8,103 @@
 namespace Admin\Controller;
 use Think\Controller;
 class NewsController extends CommonController {
+    public function newsList(){
+        if(IS_POST){
+            $news_db=D('News');
+            $list=$news_db->select();
+            $this->ajaxReturn($list);
+        }
+        else{
+
+            $this->display();
+        }
+    }
     public function categoryList(){
         if(IS_POST){
             $cate_db=D('Category');
-            $data=$cate_db->getTree();
+            if(S('news_categorylist')){
+                $data = S('news_categorylist');
+            }else{
+                $data=$cate_db->getTree();
+                S('news_categorylist', $data);
+            }
+
+
             $this->ajaxReturn($data);
         }
         else{
+
             $this->display();
+        }
+    }
+    public function categoryDelete($id=0){
+        if($id && IS_POST){
+            $cate_db=D('Category');
+            if($cate_db->isHasChilder($id)){
+                $this->error('删除失败,请先删除子菜单');
+            }
+            $result = $cate_db->where(array('catid'=>$id))->delete();
+            if($result){
+                $cate_db->clearCatche();
+                $this->success('删除成功');
+            }else {
+                $this->error('删除失败');
+            }
+        }else{
+            $this->error('删除失败');
+        }
+    }
+    public function getCategorySelectTree(){
+        $cate_db=D('Category');
+        if(IS_POST){
+            if(S('news_categoryselectlist')){
+                $data = S('news_categoryselectlist');
+            }else{
+                $data = $cate_db->getSelectTree();
+                S('news_categoryselectlist', $data);
+            }
+            $this->ajaxReturn($data);
+        }
+    }
+    public function categoryEdit($id=0){
+        $cate_db=D('Category');
+        if(IS_POST){
+
+            $data = I('post.info');
+            $data['display'] = $data['display'] ? '1' : '0';
+            $result = $cate_db->where(array('catid'=>$id))->save($data);
+            if($result){
+                $cate_db->clearCatche();
+                $this->success('修改成功');
+            }else {
+                $this->error('修改失败');
+            }
+        }else{
+            if($id){
+                $list=$cate_db->where(array("catid"=>$id))->find();
+                if($list){
+                    $this->assign("info",$list);
+                }
+            }
+            $this->display();
+        }
+    }
+    public function categoryAdd($parentid=0){
+
+        $cate_db=D('Category');
+        if(IS_POST){
+            $data = I('post.info');
+            $data['display'] = $data['display'] ? '1' : '0';
+            $id = $cate_db->add($data);
+            if($id){
+                $cate_db->clearCatche();
+                $this->success('添加成功');
+            }else {
+                $this->error('添加失败');
+            }
+        }else{
+            $this->assign('parentid', $parentid);
+            $this->display('');
         }
     }
 }
--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -44,5 +44,40 @@ class CategoryModel extends Model
         }
         return $data;
     }
+
+    /**
+     * 判断是否有子菜单
+     */
+    public function isHasChilder($id){
+        $count=$this->where(array('parentid'=>$id))->count();
+        if($count){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function clearCatche(){
+        S('news_categorylist', null);
+        S('news_categoryselectlist', null);
+    }
+    /**
+     * 菜单下拉列表
+     */
+    public function getSelectTree($parentid = 0){
+        $field = array('`catid` as `id`','`catname` as `text`');
+        $order = '`listorder` ASC,`catid` DESC';
+        $data = $this->field($field)->where(array('parentid'=>$parentid,'display'=>1))->order($order)->select();
+        if (is_array($data)){
+            foreach ($data as &$arr){
+                $arr['children'] = $this->getSelectTree($arr['id']);
+            }
+        }else{
+            $data = array();
+        }
+        return $data;
+    }
+
 }
 ?>
