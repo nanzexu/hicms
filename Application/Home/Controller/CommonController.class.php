04a23fdcb57cf240ba6fdf77d2341f2ba69c48e0
许正楠 <811046@qq.com>
2016-05-08 12:56:58 +0800

    setting

---
+++ Application/Home/Controller/CommonController.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/5/6 0006
+ * Time: 12:31
+ */
+namespace Home\Controller;
+use Think\Controller;
+class CommonController extends Controller
+{
+    public $settings;
+    public $good;
+    function _initialize(){
+        //读取系统设置
+        $setting_db = M('Setting');
+        $this->settings=$setting_db->select();
+        $this->assign('settings',$this->settings);
+
+        //读取产品信息
+        $goodone_db=D("Goodone");
+        $goodinfo=$goodone_db->select();
+        foreach($goodinfo as $key=>$value){
+            if($value['name']=="item_name" or $value['name']=="item_price" or $value['name']=="item_photo" or $value['name']=="form_name"){
+                $this->good[$value['name']]=json_decode($value['value']);
+            }
+            elseif($value['name']=='remark'){
+                $this->good[$value['name']]=htmlspecialchars_decode($value['value']);
+            }
+            else{
+                $this->good[$value['name']]=$value['value'];
+            }
+        }
+        $this->assign("good",$this->good);
+    }
+}
